9ab8977e3edbe48d9e9a239e452e60d1890edb1c
Sean Davis <smd.seandavis@gmail.com>
2013-02-12 21:45:27 -0500

    Recreate old build method, remove quickly base, update doc versions to 0.6.0

---
+++ build.py
@@ -0,0 +1,61 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+### BEGIN LICENSE
+# Copyright (C) 2007-2012 Christian Dywan <christian@twotoasts.de>
+# Copyright (C) 2012-2013 Sean Davis <smd.seandavis@gmail.com>
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License version 2, as published 
+# by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranties of 
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+# PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along 
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+### END LICENSE
+
+import sys
+import os
+
+if len(sys.argv) < 2:
+    print('No arguments supplied.')
+    sys.exit(1)
+
+if sys.argv[1] == 'check':
+    print('Checking module dependencies...')
+    try:
+        import optparse, locale, hashlib, shutil, mimetypes, itertools
+        import inspect, functools, logging
+        import calendar, datetime, time
+        from xml.sax.saxutils import escape, unescape
+        from gi.repository import GObject, Gtk, Gdk, GdkPixbuf, Pango
+        import zeitgeist # optional
+
+    except ImportError as msg:
+        print (str(msg))
+        module = str(msg).split()[-1]
+        if not module in ('zeitgeist'):
+            print ('...Error: The required module %s is missing.' % module)
+            sys.exit(1)
+        else:
+            print ('...Warning: The optional module %s is missing.' % module)
+    print ('...OK')
+
+elif sys.argv[1] == 'build':
+    import py_compile
+    
+    for filename in os.listdir('catfish_lib'):
+        filename = 'catfish_lib/' + filename
+        if filename.endswith('.py'):
+            print ('Compiling %s ...' % filename)
+            py_compile.compile( filename, filename+'c' )
+            
+    for filename in os.listdir('catfish'):
+        filename = 'catfish/' + filename
+        if filename.endswith('.py'):
+            print ('Compiling %s ...' % filename)
+            py_compile.compile( filename, filename+'c' )
+
+    print ('Compiling bin/catfish ...')
+    py_compile.compile( 'bin/catfish.py', 'bin/catfish.pyc' )
