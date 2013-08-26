04d863709a8a2acf9d381c47c5a72fc8ffcd6170
Sean Davis <smd.seandavis@gmail.com>
2013-08-25 22:20:53 -0400

    Bump details to 0.8.0

--- catfish_lib/catfishconfig.py
+++ catfish_lib/catfishconfig.py
@@ -27,7 +27,7 @@ __all__ = [
 # files). By default, this is ../data, relative your trunk layout
 __catfish_data_directory__ = '../data/'
 __license__ = 'GPL-2'
-__version__ = '0.6.4'
+__version__ = '0.8.0'
 
 import os
 
