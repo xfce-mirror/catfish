6d4341ec4f2e8fa45e88ea72df93494c6258ccfe
Jackson Doak <noskcaj@ubuntu.com>
2013-11-03 12:16:27 +1100

    Make catfish_lib/helpers.py pep8 compatible

--- catfish_lib/helpers.py
+++ catfish_lib/helpers.py
@@ -3,16 +3,16 @@
 ### BEGIN LICENSE
 # Copyright (C) 2007-2012 Christian Dywan <christian@twotoasts.de>
 # Copyright (C) 2012-2013 Sean Davis <smd.seandavis@gmail.com>
-# This program is free software: you can redistribute it and/or modify it 
-# under the terms of the GNU General Public License version 2, as published 
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2, as published
 # by the Free Software Foundation.
-# 
-# This program is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranties of 
-# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
 # PURPOSE.  See the GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License along 
+#
+# You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 ### END LICENSE
 
@@ -25,10 +25,10 @@ import os
 from . catfishconfig import get_data_file
 from . Builder import Builder
 
+
 def get_builder(builder_file_name):
-    """Return a fully-instantiated Gtk.Builder instance from specified ui 
-    file
-    
+    """Return a fully-instantiated Gtk.Builder instance from specified ui file
+
     :param builder_file_name: The name of the builder file, without extension.
         Assumed to be in the 'ui' directory under the data path.
     """
@@ -55,6 +55,7 @@ class NullHandler(logging.Handler):
     def emit(self, record):
         pass
 
+
 def set_up_logging(opts):
     # add a handler to prevent basicConfig
     root = logging.getLogger()
@@ -68,7 +69,7 @@ def set_up_logging(opts):
     logger_sh = logging.StreamHandler()
     logger_sh.setFormatter(formatter)
     logger.addHandler(logger_sh)
-    
+
     search_logger = logging.getLogger('catfish_search')
     search_logger_sh = logging.StreamHandler()
     search_logger_sh.setFormatter(formatter)
@@ -87,6 +88,7 @@ def set_up_logging(opts):
         if opts.verbose > 1:
             lib_logger.setLevel(logging.DEBUG)
 
+
 def get_help_uri(page=None):
     # help_uri from source tree - default language
     here = os.path.dirname(__file__)
@@ -102,10 +104,12 @@ def get_help_uri(page=None):
 
     return help_uri
 
+
 def show_uri(parent, link):
-    from gi.repository import Gtk # pylint: disable=E0611
+    from gi.repository import Gtk  # pylint: disable=E0611
     screen = parent.get_screen()
     Gtk.show_uri(screen, link, Gtk.get_current_event_time())
+
 
 def alias(alternative_function_name):
     '''see http://www.drdobbs.com/web-development/184406073#l9'''
