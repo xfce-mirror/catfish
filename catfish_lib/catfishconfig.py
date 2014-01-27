64dd35647098e6f3f7c81319ce13009077d41f15
Sean Davis <smd.seandavis@gmail.com>
2014-01-26 22:07:12 -0500

    Cleanup license header

--- catfish_lib/catfishconfig.py
+++ catfish_lib/catfishconfig.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
-### BEGIN LICENSE
-# Copyright (C) 2007-2012 Christian Dywan <christian@twotoasts.de>
-# Copyright (C) 2012-2013 Sean Davis <smd.seandavis@gmail.com>
-# This program is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License version 2, as published
-# by the Free Software Foundation.
+#   Catfish - a versatile file searching tool
+#   Copyright (C) 2007-2012 Christian Dywan <christian@twotoasts.de>
+#   Copyright (C) 2012-2014 Sean Davis <smd.seandavis@gmail.com>
 #
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranties of
-# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
-# PURPOSE.  See the GNU General Public License for more details.
+#   This program is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License version 2, as published
+#   by the Free Software Foundation.
 #
-# You should have received a copy of the GNU General Public License along
-# with this program.  If not, see <http://www.gnu.org/licenses/>.
-### END LICENSE
+#   This program is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranties of
+#   MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+#   PURPOSE.  See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 __all__ = [
     'project_path_not_found',
