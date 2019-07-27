328bbc79e51755f515509b415fe14c3c774e2985
Sean Davis <smd.seandavis@gmail.com>
2019-07-27 07:20:45 -0400

    Update release version

--- setup.py
+++ setup.py
@@ -198,7 +198,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='catfish',
-    version='1.4.7',
+    version='1.4.8',
     license='GPL-2+',
     author='Sean Davis',
     author_email='smd.seandavis@gmail.com',
