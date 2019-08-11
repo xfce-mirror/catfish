9144a923dec0c7a822161d69ef5bc601a99f2746
Sean Davis <smd.seandavis@gmail.com>
2019-08-11 13:55:59 -0400

    Catfish 1.4.9

--- setup.py
+++ setup.py
@@ -198,7 +198,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='catfish',
-    version='1.4.8',
+    version='1.4.9',
     license='GPL-2+',
     author='Sean Davis',
     author_email='bluesabre@xfce.org',
