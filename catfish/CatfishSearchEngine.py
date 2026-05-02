xfce-mirror/catfish

commit bdad991130f36ce62ad65fd010d7abed26871fe1

Sanitize regex passed to locate
Fixes: #148

catfish/CatfishSearchEngine.py
@@ -59,7 +59,7 @@ LOCATE_SUPPORT = which('locate') is not None
 
 
 def get_keyword_list(keywords):
-    keywords = keywords.replace(",", " ").strip().lower()
+    keywords = ' '.join(keywords.replace(",", " ").strip().lower().split())
     kwords = []
     matches = re.findall(r'\"(.+?)\"', keywords)
     for match in matches:
@@ -88,7 +88,7 @@ def string_regex(keywords, path):  # pylint: disable=W0613
             if i == 0:
                 string = p[i]
             else:
-                string += "(.)*" + p[i]
+                string += ".*" + p[i]
         regex += string
         count += 1
 

commit b005323c986fb732d4954d98eee4cc7482aa5bea

Fix previous commit
It was breaking the search for strings containing multiple spaces.

Fixes: bdad991130f36ce62ad65fd010d7abed26871fe1
Related: #148

catfish/CatfishSearchEngine.py
@@ -59,13 +59,15 @@ LOCATE_SUPPORT = which('locate') is not None
 
 
 def get_keyword_list(keywords):
-    keywords = ' '.join(keywords.replace(",", " ").strip().lower().split())
+    keywords = keywords.replace(",", " ").strip().lower()
     kwords = []
     matches = re.findall(r'\"(.+?)\"', keywords)
     for match in matches:
         newmatch = match.replace(" ", "\0")
         newmatch = newmatch.replace("\"", "")
         keywords = keywords.replace("\"%s\"" % match, newmatch)
+
+    keywords = " ".join(keywords.split())
     for keyword in keywords.split(" "):
         kwords.append(keyword.replace("\0", " "))
     return kwords