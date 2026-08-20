d1d7055bbdf0dc9240df675c61a7f35e7f5fc00d
Darin <newhoa@protonmail.com>
2026-08-20 10:38:00 -0400

    Add Ctrl+Enter for Show in File Manager
    Add Ctrl+Enter keyboard shortcut to
    show selected files in the file manager.
    
    Fixes #146

--- catfish/CatfishWindow.py
+++ catfish/CatfishWindow.py
@@ -2003,6 +2003,9 @@ class CatfishWindow(Window):
             self.on_menu_delete_activate(None)
         if "Control" in self.keys_pressed and "Shift" in self.keys_pressed and "C" in self.keys_pressed:
             self.on_menu_copy_location_activate(None)
+        if "Control" in self.keys_pressed and "Return" in self.keys_pressed:
+            sel = treeview.get_selection()
+            self.on_menu_filemanager_activate(sel)
         return False
 
     def new_column(self, label, colid):
